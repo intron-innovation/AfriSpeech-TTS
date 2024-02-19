97175a86e8304ca843c68612ca9f13dec1c03645
OGUN Sewade Olaolu <sewade-olaolu.ogun@inria.fr>
2024-02-19 01:29:09 +0100

    Add speaker interpolation

--- src/vits/models.py
+++ src/vits/models.py
@@ -500,6 +500,10 @@ class SynthesizerTrn(nn.Module):
     x, m_p, logs_p, x_mask = self.enc_p(x, x_lengths)
     if self.n_speakers > 0:
       g = self.emb_g(sid).unsqueeze(-1) # [b, h, 1]
+      if isinstance(sid, list):
+        # average embeddings
+        g = torch.mean(g, dim=0).unsqueeze(0) # [b, h, 1]
+    
     else:
       g = None
 
